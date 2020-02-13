6dd4c84ee0985a355ee1de5636f0a56440846890
Jethro Glaudin <jethro_glaudin@my.uri.edu>
2020-02-12 21:25:04 -0500

    updated server.js

--- server.js
+++ server.js
@@ -1,20 +1,30 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const morgan = require('morgan');
+const helmet = require("helmet");
+const cookieParser = require('cookie-parser');
+const dotenv = require('dotenv');
 const users = require('./Routes/users')
 const app = express();
 const PORT = process.env.PORT || 4000;
 
+dotenv.config({ path: "./config/config.env" })
+
 app.use(express.json());
+app.use(helmet());
+app.use(cookieParser());
 app.use('api/users', users);
 
-app.listen(PORT, () => console.log(`Currently listening on PORT ${PORT}`));
+if(app.get('env') === 'development'){
+    app.use(morgan('dev'));
+    console.log('Morgan is enabled');
+}
 
-
-mongoose.connect('mongodb://localhost/hackerNews', { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
 .then(console.log("Connected to MongDB database"))
 .catch((err) => console.log(err));
 
+app.listen(PORT, () => console.log(`Currently listening on PORT ${PORT} in ${process.env.NODE_ENV} mode`));
 
 
 
