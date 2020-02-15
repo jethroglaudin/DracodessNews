9eb8eff6a764800de4b9a2062579a0639c488ca5
Jethro Glaudin <jethro_glaudin@my.uri.edu>
2020-02-14 23:23:58 -0500

    updated validate function for password2

--- Models/User.js
+++ Models/User.js
@@ -22,7 +22,7 @@ const UserSchema = new mongoose.Schema({
         type: String,
         required: true,
         minlength: 8,
-        maxlength: 50,
+        maxlength: 100,
         unique: true
     },
     avatar: {
@@ -37,9 +37,10 @@ function validateUser(user){
     // validate User from MongoDB
    const schema = {
        name: Joi.string().min(1).max(200).required(),
-       username: Joi.string().min(2).max(15).required(),
+       userName: Joi.string().min(2).max(15).required(),
        email: Joi.string().required().email(),
-       password: Joi.string().min(8).max(50).required()
+       password: Joi.string().min(8).max(100).required(),
+       password2: Joi.string()
    }
    return Joi.validate(user, schema);
 }
