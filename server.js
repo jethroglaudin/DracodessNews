5e2694d096b4c91ac083276c1bc1b0fe2f3b1fb0
Jethro Glaudin <jethro_glaudin@my.uri.edu>
2020-02-16 10:01:28 -0500

    imported post routes and called app.use to initialize those routes

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const helmet = require("helmet");
 const cookieParser = require('cookie-parser');
 const dotenv = require('dotenv');
 const users = require('./Routes/users')
+const post = require('./Routes/post');
 const app = express();
 const PORT = process.env.PORT || 4000;
 
@@ -16,6 +17,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(helmet());
 app.use(cookieParser());
 app.use('/api/users/', users);
+app.use('/api/post/', post);
 
 if(app.get('env') === 'development'){
     app.use(morgan('dev'));
@@ -32,4 +34,9 @@ mongoose.connect(process.env.MONGO_URI, {
 
 app.listen(PORT, () => console.log(`Currently listening on PORT ${process.env.PORT} in ${process.env.NODE_ENV} mode`));
 
+
+process.on("unhandledRejection", (err, promise) => {
+    console.log(`Error: ${error.message}`);
+    server.close(() => process.exit(1));
+})
 
