4a48b4f0a4a151b2fb41ef792cd36965ca161387
Jethro Glaudin <jethro_glaudin@my.uri.edu>
2020-02-17 10:24:13 -0500

    updated server.js to handle unhandled rejections

--- server.js
+++ server.js
@@ -32,10 +32,10 @@ mongoose.connect(process.env.MONGO_URI, {
 .then(console.log("Connected to MongDB database"))
 .catch((err) => console.log(err));
 
-app.listen(PORT, () => console.log(`Currently listening on PORT ${process.env.PORT} in ${process.env.NODE_ENV} mode`));
+const server = app.listen(PORT, () => console.log(`Currently listening on PORT ${process.env.PORT} in ${process.env.NODE_ENV} mode`));
 
 
-process.on("unhandledRejection", (err, promise) => {
+process.on("unhandledRejection", (error, promise) => {
     console.log(`Error: ${error.message}`);
     server.close(() => process.exit(1));
 })
